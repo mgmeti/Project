5051ce23d3b574797eb33d9eddaacaecb3096c09
mgmeti <mgmeti18@gmail.com>
2024-12-17 19:13:53 +0530

    Unused method is removed from QuestionRepository

--- src/main/java/com/quiz/app/repository/QuestionRepository.java
+++ src/main/java/com/quiz/app/repository/QuestionRepository.java
@@ -9,9 +9,6 @@ import com.quiz.app.model.Question;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    // Example: Find a random question (custom native SQL query if needed)
-    // This assumes you use SQL RANDOM() to get random rows from the database
-    Question findTopByOrderByIdDesc();
-
+   
 	List<Question> findAll();
 }
